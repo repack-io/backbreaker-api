4c970f61ce7d004e976d969bd436083c6e900428
repack-io <repack.io.root@gmail.com>
2025-11-16 08:45:21 -0600

    Large commit.

---
+++ src/main/java/com/repackio/backbreaker/backbreaker/BackbreakerApplication.java
@@ -0,0 +1,14 @@
+package com.repackio.backbreaker.backbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class BackbreakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackbreakerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/repackio/backbreaker/backbreaker/BackbreakerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.repackio.backbreaker.backbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled("Disabled until proper test configuration is introduced")
+@SpringBootTest
+class BackbreakerApplicationTests {
+    @Test
+    void contextLoads() {
+        // simple smoke test
+    }
+}
